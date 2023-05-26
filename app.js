674aa1b5d69f0e8017ade3bbf2bb5d1aa1b43ea3
duela <duelapower@gmail.com>
2023-05-26 03:45:51 +0200

    Implement gitignore

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('node:https');
 const bodyParser = require('body-parser');
 const app = express();
+const apiKey = require(__dirname + "/secret/key.js");
 const port = 3005;
 const ejs = require('ejs');
 
@@ -16,18 +17,19 @@ app.get("/", function(req, res){
 
 app.post("/", function(req,res){
   var cityName = "London";
-  const apiKey = "10ef48dce66c3ece8be87a0048d04988";
+  const key = apiKey.getKey();
   const units = "metric";
   cityName = req.body.cityName;
   const cityNameCap = cityName.charAt(0).toUpperCase() + cityName.slice(1);
-  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityNameCap + "&units=" + units + "&appid=" + apiKey;
+  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+  + cityNameCap + "&units=" + units + "&appid=" + key;
   https.get(weatherUrl,function(response){
-    console.log('Status code: ' + response.statusCode);
+    //console.log('Status code: ' + response.statusCode);
     response.on('data', function(datas){
       //process.stdout.write(datas);
       //console.log(datas);
       const weatherData = JSON.parse(datas);
-      console.log(weatherData);
+      //console.log(weatherData);
 
       var nameTemp = (Math.floor(weatherData.main.temp)) + "°C";
       var today = new Date();
@@ -41,20 +43,8 @@ app.post("/", function(req,res){
       pressure = (pressure/1000) + " hPa";
       var iconId = weatherData.weather[0].icon;
       var iconUrl = 'https://api.openweathermap.org/img/w/' + iconId + '.png';
-      //  var imgSrc = 'src=' + iconUrl + "''";
 
-      // $(".weather-icon").attr("src", iconUrl);
-      // $(".city-name").text(name);
-      // $(".city-time").text("15:00");
-      // $(".city-temperature").text(nameTemp + "°C");
-      // $(".city-weather-description").text(cityDescription);
-      // $(".city-wind-speed").text(windSpeed + "km/h");
-      // $(".city-wind-humidity").text(windHumidity + "%");
-      // $(".city-pressure").text(pressure + " hPa");
-      // res.sendFile(__dirname + "/index.html");
-
-
-
+// using EJS
     res.render('index', {
        dateTime: dateTime,
        nameTemp:nameTemp,
@@ -64,26 +54,10 @@ app.post("/", function(req,res){
         windHumidity: windHumidity,
        pressure: pressure,
        iconUrl: iconUrl
-
     });
-
     });
-      //document.querySelectorAll(".wicon")[0].setAttribute("src",iconUrl);
-      //$('.wicon').attr("src",iconUrl);
-
-      //  $('.weatherStuff').text('h1Value');
-
-      //res.sendFile(__dirname + '/index.html');
     });
   });
-  // app.get('/result', function(req, res){
-  //   var today = new Date();
-  //   var dateTime = today.getHours()+":" + today.getMinutes();
-  //   res.render('index', {
-  //      dateTime: dateTime
-  //   });
-  // });
-
 
 app.listen(port, function(req,res){
   console.log("Connected to port " + port);
